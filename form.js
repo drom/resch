730179d70e98f368d3a786c20d542d4ddafbe515
Aliaksei Chapyzhenka <alex.drom@gmail.com>
2017-08-02 11:20:47 -0700

    made form understanding oneOf #5 ; allOf #13 ; anyOf #14

--- form.js
+++ form.js
@@ -18,23 +18,28 @@ module.exports = React => des => {
         const type = schema.type
             , widget = schema.widget;
 
-        if (schema.enum !== undefined) {
-            return comps.enum(config);
-        }
+        let fn;
+        if (schema.allOf !== undefined) { fn = comps.allOf; } else
+        if (schema.anyOf !== undefined) { fn = comps.anyOf; } else
+        if (schema.oneOf !== undefined) { fn = comps.oneOf; } else
 
-        if (typeof type !== 'string') {
-            throw new Error(
-                'Unxpected type: `' + (typeof type) + '`' +
-                ' of node: ' + JSON.stringify(path)
-            );
-        }
-        const key = type + (widget ? ('_' + widget) : '');
-        const fn = comps[key];
-        if (typeof fn !== 'function') {
-            throw new Error(
-                'Unexpected type: `' + key + '`' +
-                ' of node: ' + JSON.stringify(path)
-            );
+        if (schema.enum !== undefined) {
+            fn = comps.enum;
+        } else {
+            if (typeof type !== 'string') {
+                throw new Error(
+                    'Unxpected type: `' + (typeof type) + '`' +
+                    ' of node: ' + JSON.stringify(path)
+                );
+            }
+            const key = type + (widget ? ('_' + widget) : '');
+            fn = comps[key];
+            if (typeof fn !== 'function') {
+                throw new Error(
+                    'Unexpected type: `' + key + '`' +
+                    ' of node: ' + JSON.stringify(path)
+                );
+            }
         }
         return fn(config);
     }
