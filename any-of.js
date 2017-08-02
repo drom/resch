4e7de76a5eb4e5608810c0d685de38b67588a995
Aliaksei Chapyzhenka <alex.drom@gmail.com>
2017-08-02 11:19:30 -0700

    dummy anyOf ; for #14

---
+++ any-of.js
@@ -0,0 +1,30 @@
+'use strict';
+
+module.exports = React => {
+    const $ = React.createElement;
+    return genForm => {
+        /* eslint no-unused-vars: 1 */
+
+        return config => {
+
+            return class AnyOf extends React.Component {
+
+                constructor (props) {
+                    super(props);
+                }
+
+                shouldComponentUpdate (nextProps) {
+                    if (this.props.data === nextProps.data) {
+                        return false;
+                    }
+                    return true;
+                }
+
+                render () {
+                    // const data = this.props.data;
+                    return $('div', {}, 'anyOf');
+                }
+            };
+        };
+    };
+};
