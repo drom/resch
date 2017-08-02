75e3ecf70963cf7588f40641b12dd03183a5759a
Aliaksei Chapyzhenka <alex.drom@gmail.com>
2017-08-02 11:19:05 -0700

    dummy oneOf ; for #5

---
+++ one-of.js
@@ -0,0 +1,30 @@
+'use strict';
+
+module.exports = React => {
+    const $ = React.createElement;
+    return genForm => {
+        /* eslint no-unused-vars: 1 */
+
+        return config => {
+
+            return class OneOf extends React.Component {
+
+                constructor (props) {
+                    super(props);
+                }
+
+                shouldComponentUpdate (nextProps) {
+                    if (this.props.data === nextProps.data) {
+                        return false;
+                    }
+                    return true;
+                }
+
+                render () {
+                    // const data = this.props.data;
+                    return $('div', {}, 'oneOf');
+                }
+            };
+        };
+    };
+};
