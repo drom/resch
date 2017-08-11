5f988f59268a829475f8ef2f709a3277d6d6134e
Aliaksei Chapyzhenka <alex.drom@gmail.com>
2017-08-10 23:25:47 -0700

    array: minItems, maxItems ; implements #15

---
+++ test/validate-array.js
@@ -0,0 +1,37 @@
+'use strict';
+
+const React = require('react')
+    , ReactDOMServer = require('react-dom/server')
+    , chai = require('chai')
+    , resch = require('../lib/')
+    ;
+
+const expect = chai.expect;
+
+describe('array errors', () => {
+    const $ = React.createElement;
+    const desc = Object.assign({}, resch);
+    const genForm = resch.__form(React)(desc);
+
+    [[], [1,2,3], [1,2,3,4,5,6]]
+        .forEach(data => {
+            it(JSON.stringify(data), done => {
+                const Form = genForm({ schema: {
+                    type: 'array',
+                    minItems: 2,
+                    maxItems: 4,
+                    items: {
+                        type: 'number'
+                    }
+                }});
+                const res = ReactDOMServer.renderToStaticMarkup($(Form, {
+                    data: data
+                }));
+                expect(res).to.be.a('string');
+                done();
+            });
+        });
+
+});
+
+/* eslint-env mocha */
